e4fbd23eed9b5348b3a09f8c4035bb03f7d069ac
Shahid Shah <shahid@shah.org>
2019-05-14 22:24:40 -0400

    Rename all hooks with 'On' prefix

--- dropmark_test.go
+++ dropmark_test.go
@@ -1,20 +1,33 @@
 package dropmark
 
 import (
+	"context"
 	"io"
+	"net/http"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/suite"
 )
 
 type DropmarkSuite struct {
 	suite.Suite
+	httpClient *http.Client
 }
 
 func (suite *DropmarkSuite) SetupSuite() {
+	suite.httpClient = &http.Client{Timeout: time.Second * 66}
 }
 
 func (suite *DropmarkSuite) TearDownSuite() {
+}
+
+func (suite *DropmarkSuite) HTTPClient() *http.Client {
+	return suite.httpClient
+}
+
+func (suite *DropmarkSuite) OnPrepareHTTPRequest(client *http.Client, req *http.Request) {
+	req.Header.Set("User-Agent", "github.com/lectio/dropmark.DropmarkSuite")
 }
 
 func (suite *DropmarkSuite) StartReportableReaderActivityInBytes(summary string, exepectedBytes int64, inputReader io.Reader) io.Reader {
@@ -26,13 +39,15 @@ func (suite *DropmarkSuite) CompleteReportableActivityProgress(summary string) {
 }
 
 func (suite *DropmarkSuite) TestDropmarkCollection() {
-	collection, getErr := GetCollection("https://shah.dropmark.com/652682.json", suite, HTTPUserAgent, HTTPTimeout)
+	ctx := context.Background()
+	collection, getErr := GetCollection(ctx, "https://shah.dropmark.com/652682.json", suite)
 	suite.Nil(getErr, "Unable to retrieve Dropmark collection from %q: %v.", collection.apiEndpoint, getErr)
 	suite.Equal(len(collection.Items), 4)
 }
 
 func (suite *DropmarkSuite) TestContent() {
-	collection, getErr := GetCollection("https://shah.dropmark.com/652682.json", suite, HTTPUserAgent, HTTPTimeout)
+	ctx := context.Background()
+	collection, getErr := GetCollection(ctx, "https://shah.dropmark.com/652682.json")
 	suite.Nil(getErr, "Unable to retrieve Dropmark collection from %q: %v.", collection.apiEndpoint, getErr)
 	count, getItemFn, contentErr := collection.Content()
 	suite.Nil(contentErr, "Unable to get Dropmark content iterator from %q: %v.", collection.apiEndpoint, contentErr)
@@ -42,7 +57,7 @@ func (suite *DropmarkSuite) TestContent() {
 	genericItem1, genericItem1Err := getItemFn(1, 1)
 	suite.Nil(genericItem1Err, "Unable to get Dropmark content item from %q: %v.", collection.apiEndpoint, genericItem1Err)
 	item1 := genericItem1.(*Item)
-	suite.Equal("https://www2.deloitte.com/insights/us/en/industry/financial-services/demystifying-cybersecurity-insurance.html", item1.TargetURL())
+	suite.Equal("https://www2.deloitte.com/insights/us/en/industry/financial-services/demystifying-cybersecurity-insurance.html", item1.OriginalURL())
 
 	// get a range of items -- the item function will return a slice
 	genericItems, genericItemsErr := getItemFn(1, 3)
@@ -52,7 +67,8 @@ func (suite *DropmarkSuite) TestContent() {
 }
 
 func (suite *DropmarkSuite) TestInvalid() {
-	_, getErr := GetCollection("https://sha.dropmark.com/652682.json", suite, HTTPUserAgent, HTTPTimeout)
+	ctx := context.Background()
+	_, getErr := GetCollection(ctx, "https://sha.dropmark.com/652682.json")
 	suite.NotNil(getErr, "Should be an error", getErr)
 }
 
